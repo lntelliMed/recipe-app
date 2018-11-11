c3ec342b7377cc77e0c034fcb7394857620880e6
“Sam <sam.alsmadi@gmail.com>
2018-11-11 14:15:01 -0500

    Add one to many relationship between Recipe and Ingredient

--- src/main/java/com/lntellimed/recipe/domain/Recipe.java
+++ src/main/java/com/lntellimed/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.lntellimed.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,7 +17,11 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
+
 	// private Difficulty difficulty;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
 
 	@Lob
 	private Byte[] image;
@@ -102,5 +107,13 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 }
