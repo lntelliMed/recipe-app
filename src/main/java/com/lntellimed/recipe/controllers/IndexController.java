5628251798b402f21f09b19993840dd803b0e337
“Sam <sam.alsmadi@gmail.com>
2018-11-19 23:29:16 -0500

    Add query methods for category and unit of measure repos

--- src/main/java/com/lntellimed/recipe/controllers/IndexController.java
+++ src/main/java/com/lntellimed/recipe/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.lntellimed.recipe.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lntellimed.recipe.domain.Category;
+import com.lntellimed.recipe.domain.UnitOfMeasure;
+import com.lntellimed.recipe.repositories.CategoryRepository;
+import com.lntellimed.recipe.repositories.UnitOfMeasureRepository;
+
 @Controller
 public class IndexController {
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        return "index";
-    }
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage() {
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		System.out.println("Cat Id is: " + categoryOptional.get().getId());
+		System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+		
+		return "index";
+	}
 }
--- src/main/java/com/lntellimed/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/lntellimed/recipe/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,11 @@
 package com.lntellimed.recipe.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.lntellimed.recipe.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
-
+	Optional<UnitOfMeasure> findByDescription(String description);
 }
