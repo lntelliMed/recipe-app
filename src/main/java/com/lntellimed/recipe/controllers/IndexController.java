5c1bfc8d853caa5d98b19cbc2103dab94f6636ad
Sam Alsmadi <sam.alsmadi@gmail.com>
2018-12-16 15:05:45 -0500

    refact: use Lombok annotations wherever possible

--- src/main/java/com/lntellimed/recipe/controllers/IndexController.java
+++ src/main/java/com/lntellimed/recipe/controllers/IndexController.java
@@ -5,18 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.lntellimed.recipe.services.RecipeService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class IndexController {
 
-	private final RecipeService recipeService;
+    private final RecipeService recipeService;
 
-	public IndexController(RecipeService recipeService) {
-		this.recipeService = recipeService;
-	}
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
-	@RequestMapping({ "", "/", "/index" })
-	public String getIndexPage(Model model) {
-		model.addAttribute("recipes", recipeService.getRecipes());
-		return "index";
-	}
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        log.debug("Getting Index page");
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+        return "index";
+    }
 }
