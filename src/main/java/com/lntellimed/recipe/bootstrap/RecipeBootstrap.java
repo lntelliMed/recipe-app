8ed58ef72f6a1e6d6366850de4aea1d9374f4455
Sam Alsmadi <sam.alsmadi@gmail.com>
2018-12-29 16:10:48 -0500

    Add transactional to bootstrap method

--- src/main/java/com/lntellimed/recipe/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/lntellimed/recipe/bootstrap/RecipeBootstrap.java
@@ -10,11 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 
 @Slf4j
 @Component
@@ -32,6 +34,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 	}
 
 	@Override
+	@Transactional
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		recipeRepository.saveAll(getRecipes());
 		log.debug("Loading Bootstrap Data");
