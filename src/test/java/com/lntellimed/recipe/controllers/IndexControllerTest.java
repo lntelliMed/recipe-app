ecf8efb7eaaed75a149b44fd1de7f8611d82b297
Sam Alsmadi <sam.alsmadi@gmail.com>
2019-01-01 21:15:29 +0200

    Add Spring Mock MVC testing example

--- src/test/java/com/lntellimed/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/lntellimed/recipe/controllers/IndexControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.lntellimed.recipe.domain.Recipe;
@@ -15,7 +17,13 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+/**
+ * Created by jt on 6/17/17.
+ */
 public class IndexControllerTest {
 
 	@Mock
@@ -34,7 +42,15 @@ public class IndexControllerTest {
 	}
 
 	@Test
+	public void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+	}
+
+	@Test
 	public void getIndexPage() throws Exception {
+
 		// given
 		Set<Recipe> recipes = new HashSet<>();
 		recipes.add(new Recipe());
