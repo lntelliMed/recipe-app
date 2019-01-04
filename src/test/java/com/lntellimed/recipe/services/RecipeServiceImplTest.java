c962dacba12a508f9e0732a997fed30f5ad2da59
Sam Alsmadi <sam.alsmadi@gmail.com>
2019-01-04 20:09:31 +0200

    View recipe by ID

--- src/test/java/com/lntellimed/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/com/lntellimed/recipe/services/RecipeServiceImplTest.java
@@ -9,9 +9,11 @@ import com.lntellimed.recipe.domain.Recipe;
 import com.lntellimed.recipe.repositories.RecipeRepository;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
@@ -29,18 +31,33 @@ public class RecipeServiceImplTest {
 	}
 
 	@Test
-	public void getRecipes() throws Exception {
-
+	public void getRecipeByIdTest() throws Exception {
 		Recipe recipe = new Recipe();
-		HashSet recipesData = new HashSet();
-		recipesData.add(recipe);
+		recipe.setId(1L);
+		Optional<Recipe> recipeOptional = Optional.of(recipe);
 
-		when(recipeRepository.findAll()).thenReturn(recipesData);
+		when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+		Recipe recipeReturned = recipeService.findById(1L);
+
+		assertNotNull("Null recipe returned", recipeReturned);
+		verify(recipeRepository, times(1)).findById(anyLong());
+		verify(recipeRepository, never()).findAll();
+	}
+
+	@Test
+	public void getRecipesTest() throws Exception {
+		Recipe recipe = new Recipe();
+		HashSet receipesData = new HashSet();
+		receipesData.add(recipe);
+
+		when(recipeService.getRecipes()).thenReturn(receipesData);
 
 		Set<Recipe> recipes = recipeService.getRecipes();
 
 		assertEquals(recipes.size(), 1);
 		verify(recipeRepository, times(1)).findAll();
+		verify(recipeRepository, never()).findById(anyLong());
 	}
 
 }
